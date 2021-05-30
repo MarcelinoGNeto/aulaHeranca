097d52ec785cd742a1787406d94bd43881ea4181
Marcelino Neto <marcelino.gneto@gmail.com>
2021-05-30 17:11:43 -0300

    Aplicando conceitos de Upcasting e Downcasting

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,5 +1,9 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
 public class Programa {
 
 	public static void main(String[] args) {
@@ -13,6 +17,31 @@ public class Programa {
  tem tudo que a classe account tem e ainda extende outras funções
  * Herança é uma associação entre classes (e não entre objetos)
  */
+
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		//UPCASTING: converter objetos das subclasses na superclasse
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new BusinessAccount(1004, "Anna", 0.0, 0.01);
+		
+		// DOWNCAST: converter objetos da superclasse nas subclasses
+		BusinessAccount acc4 = (BusinessAccount)acc2; //(BusinessAccount) permite o acesso da subclasse na superclasse
+		acc4.loan(100.0);
+		
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");//trad.: Loan -> empréstimmo
+		}
+		
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
 
 	}
 
---
+++ src/entities/SavingsAccount.java
@@ -0,0 +1,30 @@
+package entities;
+
+public class SavingsAccount extends Account { //extends: herdando da classe Account
+	//trad: Poupança
+	
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+//métodos exclusivos da classe
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+
+}
